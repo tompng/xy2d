910f272891537b43d8b6cbe62a6a572af3313d4e
tompng <tomoyapenguin@gmail.com>
2021-10-04 00:35:28 +0900

    input

--- src/3d/index.ts
+++ src/3d/index.ts
@@ -4,25 +4,39 @@ import { Range3D, splitRanges, polygonize } from './polygonizer'
 import { View, generateGeometry, generateMesh } from './view'
 import type { BufferGeometry } from 'three'
 
-onload = polygonizeTest
 function sleep(millisec: number) {
   return new Promise(r => setTimeout(r, millisec))
 }
 
-async function polygonizeTest() {
-  const exp = 'sin(5xy+1)+sin(5yz+2)+sin(5zx+3)-1/2=0'
+// other samples: max(x^4+y^4+z^4,abs(x)+abs(y)+abs(z)-1)=0.5
+
+const view = new View()
+view.setSize(800, 600)
+document.body.appendChild(view.renderer.domElement)
+
+const errorDOM = document.querySelector<HTMLDivElement>('#error')!
+const input = document.querySelector<HTMLInputElement>('#mathinput')!
+async function update() {
+  const exp = input.value
+  try {
+    await calc(exp)
+  } catch (e) {
+    errorDOM.textContent = String(e)
+  }
+}
+input.onchange = update
+input.oninput = () => {
+  errorDOM.textContent = ''
+}
+update()
+
+let geometry: BufferGeometry | null = null
+async function calc(exp: string) {
   let [ast, mode] = parse(exp)
   const frange = astTo3DRangeFunction(ast, { pos: false, neg: false })
   const fvalue = astTo3DFunction(ast)
   let ranges: Range3D[] = [[-1,1,-1,1,-1,1]]
-
-  const view = new View()
-  view.setSize(800, 600)
-  document.body.appendChild(view.renderer.domElement)
-
-  let geometry: BufferGeometry | null = null
-
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 6; i++) {
     ranges = splitRanges(frange, ranges)
     const polygon = polygonize(fvalue, ranges, 4)
     geometry?.dispose()
