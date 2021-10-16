2a1d1c7db6ee9ce61193b26dff7b49aeec7d6f10
tompng <tomoyapenguin@gmail.com>
2021-10-17 04:07:06 +0900

    drag change formarea

--- src/3d/components/Form.tsx
+++ src/3d/components/Form.tsx
@@ -1,14 +1,99 @@
-import React, { useState, useRef, useCallback } from 'react'
+import React, { useState, useRef, useReducer, useEffect, useCallback } from 'react'
 import { List, ListItem, ListItemAvatar, Avatar, TextField, ListItemText } from '@mui/material'
 import { DndContext, DragEndEvent, closestCenter, PointerSensor, useSensor, useSensors } from '@dnd-kit/core'
-import { arrayMove, SortableContext, sortableKeyboardCoordinates, verticalListSortingStrategy, useSortable } from '@dnd-kit/sortable'
+import { arrayMove, SortableContext, verticalListSortingStrategy, useSortable } from '@dnd-kit/sortable'
 
+function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({ width: innerWidth, height: innerHeight })
+  const ref = useRef(windowSize)
+  useEffect(() => {
+    const onResize = () => {
+      if (innerWidth === ref.current.width && innerHeight === ref.current.height) return
+      ref.current.width = innerWidth
+      ref.current.height = innerHeight
+      setWindowSize({ ...ref.current })
+    }
+    const timer = setInterval(onResize, 1000) // avoid resize & windowsize bug on iOS
+    window.addEventListener('resize', onResize)
+    return () => {
+      clearInterval(timer)
+      window.removeEventListener('resize', onResize)
+    }
+  }, [])
+  return windowSize
+}
+
+function formulaAreaInitialHeight() {
+  return Math.min(innerHeight - 100, Math.max(innerHeight / 3, 300))
+}
 export const Form = (): JSX.Element => {
+  const [height, setHeight] = useState(formulaAreaInitialHeight)
+  const windowSize = useWindowSize()
+  useEffect(() => setHeight(h => Math.min(h, windowSize.height - 100)), [windowSize])
+  const onDragMove = useCallback((y: number, complete: boolean) => {
+    let h = -y
+    if (h < 0 || (complete && h < 30)) h = 0
+    setHeight(Math.min(h, innerHeight - 100))
+  }, [])
   return (
-    <div style={{ position: 'fixed', left: 0, bottom: 0, width: '100%', height: '40%', background: 'white', overflow: 'auto' }}>
-      <MathList />
-    </div>
+    <>
+      <div style={{ position: 'fixed', left: 0, bottom: 0, width: '100%', height, background: 'white', overflow: 'auto' }}>
+        <MathList />
+      </div>
+      <AreaDragHandler y={-height} onDragMove={onDragMove}>
+        <div style={{ position: 'fixed', cursor: 'ns-resize', left: 0, bottom: height - 8, width: '100%', height: 32 }}></div>
+      </AreaDragHandler>
+      <AreaDragHandler y={-height} onDragMove={onDragMove} onClick={() => setHeight(h => h === 0 ? formulaAreaInitialHeight() : 0) }>
+        <div style={{
+          position: 'fixed', cursor: 'pointer', left: 8, bottom: height + 8, width: 40, height: 40,
+          borderRadius: '50%',
+          background: 'white',
+          boxShadow: '1px 1px 2px rgba(0, 0, 0, 0.5)'
+        }}>
+        </div>
+      </AreaDragHandler>
+    </>
   )
+}
+
+type AreaDragHanderParams = {
+  y: number
+  onDragMove: (y: number, complete: boolean) => void
+  onClick?: () => void
+}
+const AreaDragHandler: React.FC<AreaDragHanderParams> = ({ y, onDragMove, onClick, children }) => {
+  const callbackRef = useRef({ onDragMove, onClick })
+  const pointerRef = useRef({ id: -1, y: 0, maxMove: 0, pageY: 0 })
+  useEffect(() => { callbackRef.current = { onClick, onDragMove } }, [onClick, onDragMove])
+  useEffect(() => {
+    const handle = (e: PointerEvent, complete: boolean) => {
+      e.preventDefault()
+      const p = pointerRef.current
+      p.maxMove = Math.max(p.maxMove, Math.abs(e.pageY - p.pageY))
+      callbackRef.current.onDragMove(p.y + e.pageY - p.pageY, complete)
+    }
+    const move = (e: PointerEvent) => {
+      if (pointerRef.current.id !== e.pointerId) return
+      handle(e, false)
+    }
+    const up = (e: PointerEvent) => {
+      if (pointerRef.current.id !== e.pointerId) return
+      pointerRef.current.id = -1
+      handle(e, true)
+      if (pointerRef.current.maxMove < 10) callbackRef.current.onClick?.()
+    }
+    document.addEventListener('pointermove', move)
+    document.addEventListener('pointerup', up)
+    return () => {
+      document.removeEventListener('pointermove', move)
+      document.removeEventListener('pointerup', up)
+    }
+  }, [])
+  const onPointerDown = useCallback((e: React.PointerEvent) => {
+    e.preventDefault()
+    pointerRef.current = { id: e.pointerId, y, maxMove: 0, pageY: e.pageY }
+  }, [y])
+  return <div onPointerDown={onPointerDown} style={{ touchAction: 'none' }}>{children}</div>
 }
 
 type MathListItemProps = {
@@ -115,7 +200,7 @@ const ClickableInsideDND: React.FC<{ onClick?: () => void }> = ({ onClick, child
   const onPointerUp = useCallback((e: React.PointerEvent) => {
     const down = ref.current
     if (down.id !== e.pointerId || performance.now() - ref.current.time > 200) return
-    if (Math.hypot(e.pageX - down.x, e.pageY - down.y) < 20) onClick?.()
+    if (Math.hypot(e.pageX - down.x, e.pageY - down.y) < 10) onClick?.()
 
   }, [onClick])
   return <div onPointerDown={onPointerDown} onPointerUp={onPointerUp} style={{ cursor: 'pointer' }}>{children}</div>
